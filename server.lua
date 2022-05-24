e56d10737059173571c1f1f5be71e06181f9cffa
TRClassic <82931913+trclassic92@users.noreply.github.com>
2022-05-24 19:04:09 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,6 +4,17 @@ QBCore.Functions.CreateUseableItem("radio", function(source, item)
     TriggerClientEvent('qb-radio:use', source)
 end)
 
+QBCore.Functions.CreateCallback('qb-radio:radiocheck', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player ~= nil then
+        if Player.Functions.GetItemByName("radio") ~= nil then
+            cb(true)
+        else
+            cb(false)
+        end
+    end
+end)
+
 for channel, config in pairs(Config.RestrictedChannels) do
     exports['pma-voice']:addChannelCheck(channel, function(source)
         local Player = QBCore.Functions.GetPlayer(source)
