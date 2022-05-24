78cb2780271b25e6767f714e5a6430a4d0fed2e6
TRClassic <82931913+trclassic92@users.noreply.github.com>
2022-05-24 14:36:31 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,6 +4,7 @@ local radioMenu = false
 local onRadio = false
 local RadioChannel = 0
 local RadioVolume = 50
+local hasRadio = false
 
 --Function
 local function LoadAnimDic(dict)
@@ -88,21 +89,50 @@ local function IsRadioOn()
     return onRadio
 end
 
+
+local function DoRadioCheck(PlayerItems)
+    local _hasRadio = false
+
+    for _, item in pairs(PlayerItems) do
+        if item.name == "radio" then
+            _hasRadio = true
+            break;
+        end
+    end
+
+    hasRadio = _hasRadio
+end
+
 --Exports
 exports("IsRadioOn", IsRadioOn)
 
 --Events
-AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+
+-- Handles state right when the player selects their character and location.
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     PlayerData = QBCore.Functions.GetPlayerData()
+    DoRadioCheck(PlayerData.items)
 end)
 
+-- Resets state on logout, in case of character change.
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    DoRadioCheck({})
     PlayerData = {}
     leaveradio()
 end)
 
+-- Handles state when PlayerData is changed. We're just looking for inventory updates.
 RegisterNetEvent('QBCore:Player:SetPlayerData', function(val)
     PlayerData = val
+    DoRadioCheck(PlayerData.items)
+end)
+
+-- Handles state if resource is restarted live.
+AddEventHandler('onResourceStart', function(resource)
+    if GetCurrentResourceName() == resource then
+        PlayerData = QBCore.Functions.GetPlayerData()
+        DoRadioCheck(PlayerData.items)
+    end
 end)
 
 RegisterNetEvent('qb-radio:use', function()
@@ -198,12 +228,12 @@ CreateThread(function()
         Wait(1000)
         if LocalPlayer.state.isLoggedIn and onRadio then
             QBCore.Functions.TriggerCallback('qb-radio:server:GetItem', function(hasItem)
-                if not hasItem then
+                if not hasItem or PlayerData.metadata.isdead or PlayerData.metadata.inlaststand then
                     if RadioChannel ~= 0 then
                         leaveradio()
                     end
                 end
-            end, "radio")
+            end
         end
     end
 end)
