86d254d11d24eb06fcfce2c70bc9c8aefd5986ed
TRClassic <82931913+trclassic92@users.noreply.github.com>
2023-03-16 22:53:48 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,7 +7,7 @@ end)
 QBCore.Functions.CreateCallback('qb-radio:radiocheck', function(source, cb)
     local Player = QBCore.Functions.GetPlayer(source)
     if Player ~= nil then
-        if Player.Functions.GetItemByName("radio") ~= nil then
+        if Player.Functions.GetItemByName("radio") ~= nil and not Player.PlayerData.metadata["isdead"] and not Player.PlayerData.metadata["inlaststand"] then
             cb(true)
         else
             cb(false)
@@ -18,6 +18,6 @@ end)
 for channel, config in pairs(Config.RestrictedChannels) do
     exports['pma-voice']:addChannelCheck(channel, function(source)
         local Player = QBCore.Functions.GetPlayer(source)
-        return config[Player.PlayerData.job.name] and Player.PlayerData.job.onduty
+        return config[Player.PlayerData.job.name]
     end)
 end
