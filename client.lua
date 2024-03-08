802deca3b8cff67c36f16cb8642df14df6c998b1
TRClassic <82931913+trclassic92@users.noreply.github.com>
2024-03-08 14:36:23 -0600

    propstuck command
    Added a command for stuck radios

--- client.lua
+++ client.lua
@@ -491,3 +491,13 @@ end)
 RegisterNetEvent('qb-radio:client:JoinRadioChannelToggle', function()
     leaveradio()
 end)
+
+RegisterCommand('propstuck', function()
+    for k, v in pairs(GetGamePool('CObject')) do
+        if IsEntityAttachedToEntity(PlayerPedId(), v) then
+            SetEntityAsMissionEntity(v, true, true)
+            DeleteObject(v)
+            DeleteEntity(v)
+        end
+    end
+end)
